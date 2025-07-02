39b7706df1eef57e6b1053152db5c218ad53951a
Your Name <ndehtaminang@gmail.com>
2025-07-02 13:07:34 +0100

    backend connection

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,10 +7,3 @@ router.post('/login', authController.loginUser);
 
 module.exports = router;
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hello from Railway!'));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
